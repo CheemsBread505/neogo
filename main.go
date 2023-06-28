e5ec57f33a2135bb573c36f4f6dbafec978f629e
Cheems Bread <CheemsBread505@proton.me>
2023-06-28 00:13:51 -0400

    More neofetch like features

--- main.go
+++ main.go
@@ -12,6 +12,28 @@ func main() {
 	fmt.Println("NeoGO")
 	fmt.Println("======")
 
+	goVersion := getGoVersion()
+	fmt.Println("Go version:", goVersion)
+
+	osName := getOperatingSystem()
+	fmt.Println("Operating System:", osName)
+
+	hostname := getHostname()
+	fmt.Println("Hostname:", hostname)
+
+	kernelVersion := getKernelVersion()
+	fmt.Println("Kernel Version:", kernelVersion)
+
+	uptime := getUptime()
+	fmt.Println("Uptime:", uptime)
+
+	shell := getShell()
+	fmt.Println("Shell:", shell)
+
+	fmt.Println("======")
+}
+
+func getGoVersion() string {
 	goVersionCmd := exec.Command("go", "version")
 	goVersionOutput, err := goVersionCmd.Output()
 	if err != nil {
@@ -20,13 +42,7 @@ func main() {
 	}
 
 	goVersion := strings.TrimSpace(string(goVersionOutput))
-	fmt.Println("Go version:", goVersion)
-
-	osName := getOperatingSystem()
-	fmt.Println("Operating System:", osName)
-
-	arch := runtime.GOARCH
-	fmt.Println("Architecture:", arch)
+	return goVersion
 }
 
 func getOperatingSystem() string {
@@ -39,9 +55,48 @@ func getOperatingSystem() string {
 
 		osRelease := string(etcOSReleaseOutput)
 		if strings.Contains(osRelease, "Arch Linux") {
-			return "archlinux"
+			return "Arch Linux"
 		}
 	}
 
 	return runtime.GOOS
+}
+
+func getHostname() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println("Error retrieving hostname:", err)
+		os.Exit(1)
+	}
+
+	return hostname
+}
+
+func getKernelVersion() string {
+	unameCmd := exec.Command("uname", "-r")
+	unameOutput, err := unameCmd.Output()
+	if err != nil {
+		fmt.Println("Error retrieving kernel version:", err)
+		os.Exit(1)
+	}
+
+	kernelVersion := strings.TrimSpace(string(unameOutput))
+	return kernelVersion
+}
+
+func getUptime() string {
+	uptimeCmd := exec.Command("uptime", "-p")
+	uptimeOutput, err := uptimeCmd.Output()
+	if err != nil {
+		fmt.Println("Error retrieving uptime:", err)
+		os.Exit(1)
+	}
+
+	uptime := strings.TrimSpace(string(uptimeOutput))
+	return uptime
+}
+
+func getShell() string {
+	shell := os.Getenv("SHELL")
+	return shell
 }
